1d8d618d9710436790719dee149267d887a5d128
Moisés Cachay <xpktro@gmail.com>
2025-05-31 22:18:46 -0500

    bump to version 0.2.0

--- src/cli/index.ts
+++ src/cli/index.ts
@@ -26,7 +26,7 @@ const program = new Command()
 program
   .name('contest-scorer')
   .description('Score ham radio contests from ADIF files')
-  .version('0.1.0')
+  .version('0.2.0')
 
 program
   .command('score')
