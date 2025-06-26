18729e29c8163029744b2d31559952aea11fa729
Moisés Cachay <xpktro@gmail.com>
2025-06-25 19:33:53 -0500

    do not execute the CLI on import

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,3 @@
-// Only run the CLI when this file is executed directly
-if (import.meta.url === `file://${process.argv[1]}`) {
-  await import('./cli')
-}
-
 // Export everything from the library for users importing this as a module
 export * from './lib'
 export type * from './types'
